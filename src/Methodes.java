2e98571c8825ab5c3c0d67ba9becdca42ccda4c7
oraculo-nl <info@oraculo.nl>
2020-10-20 15:51:07 +0200

    modified opgaves

--- src/Methodes.java
+++ src/Methodes.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Methodes {
-}
+    public static void main(String[] args) {
+
+        HelloWorld();
+        groet();
+        groet("Petra");
+    }
+
+
+    static void HelloWorld() {
+
+        System.out.println("Hello World!");
+    }
+
+    static void groet() {
+        System.out.println("Hallo, User!");
+
+    }
+
+    static void groet(String gebruiker) {
+        System.out.println(gebruiker);
+
+    }
+
+
+
+        }
+
+
+
